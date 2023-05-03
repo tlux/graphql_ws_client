29d102550c46a1ecd7c75cee730d98786f7c2120
Tobias Casper <tobias.casper@gmail.com>
2023-05-03 14:35:18 +0200

    add message to Query error

--- lib/graphql_ws_client/query_error.ex
+++ lib/graphql_ws_client/query_error.ex
@@ -2,4 +2,8 @@ defmodule GraphQLWSClient.QueryError do
   defexception errors: []
 
   @type t :: %__MODULE__{errors: [any]}
+
+  def message(%__MODULE__{} = exception) do
+    "Query error:\n\n" <> inspect(exception.errors, pretty: true)
+  end
 end
