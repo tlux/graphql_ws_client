b7807add523b5e4dcb84211919c83199e650bf9a
Tobias Casper <tobias.casper@gmail.com>
2023-05-06 17:14:07 +0200

    update driver docs

--- lib/graphql_ws_client/driver.ex
+++ lib/graphql_ws_client/driver.ex
@@ -5,26 +5,40 @@ defmodule GraphQLWSClient.Driver do
 
   alias GraphQLWSClient.{Config, Conn, Message, SocketError}
 
+  @doc """
+  Connects to the socket and returns the updated `#{inspect(Conn)}`.
+  """
   @callback connect(Conn.disconnected()) ::
               {:ok, Conn.connected()}
               | {:error, SocketError.t()}
 
+  @doc """
+  Disconnects from the socket.
+  """
   @callback disconnect(Conn.connected()) :: :ok
 
+  @doc """
+  Pushes a message to the socket.
+  """
   @callback push_message(Conn.connected(), msg :: any) :: :ok
 
+  @doc """
+  Parses a message received from the socket.
+  """
   @callback handle_message(Conn.connected(), msg :: any) ::
               {:ok, Message.t()} | {:error, SocketError.t()} | :ignore
 
   @doc false
   @spec connect(Config.t()) ::
           {:ok, Conn.connected()} | {:error, SocketError.t()}
-  def connect(%Config{driver: {driver_mod, driver_opts}} = config) do
-    driver_mod.connect(%Conn{config: config, opts: Map.new(driver_opts)})
-  end
+  def connect(%Config{driver: driver} = config) do
+    {driver_mod, driver_opts} =
+      case driver do
+        {mod, opts} -> {mod, Map.new(opts)}
+        mod -> {mod, %{}}
+      end
 
-  def connect(%Config{driver: driver_mod} = config) do
-    driver_mod.connect(%Conn{config: config, opts: %{}})
+    driver_mod.connect(%Conn{config: config, opts: driver_opts})
   end
 
   @doc false
