f43ff689784cbe61a02464adf928e679d8464986
Tobias Casper <tobias.casper@gmail.com>
2023-05-08 14:34:18 +0200

    change min Elixir version to 1.13

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule GraphQLWSClient.MixProject do
     [
       app: :graphql_ws_client,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
