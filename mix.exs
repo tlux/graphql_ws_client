8ce3b71348bec5845478db710115490bb484cb37
Tobias Casper <tobias.casper@gmail.com>
2023-05-09 18:25:29 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule GraphQLWSClient.MixProject do
   use Mix.Project
 
   @github_url "https://github.com/tlux/graphql_ws_client"
-  @version "0.1.1"
+  @version "0.1.2"
 
   def project do
     [
