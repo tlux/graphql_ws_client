a9b0894bcd3be29d75aba25c723d1a30f6ee7ecf
Tobias Casper <tobias.casper@gmail.com>
2025-04-03 16:50:13 +0200

    fix: add subprotocol header when upgrading connection

--- lib/graphql_ws_client/drivers/gun.ex
+++ lib/graphql_ws_client/drivers/gun.ex
@@ -79,7 +79,10 @@ defmodule GraphQLWSClient.Drivers.Gun do
     with {:await_up, {:ok, _protocol}} <-
            {:await_up,
             opts.adapter.await_up(pid, opts.connect_options.connect_timeout)},
-         stream_ref = opts.adapter.ws_upgrade(pid, config.path),
+         stream_ref =
+           opts.adapter.ws_upgrade(pid, config.path, [
+             {"sec-websocket-protocol", "graphql-transport-ws"}
+           ]),
          :ok <- await_upgrade(opts.upgrade_timeout),
          :ok <-
            init_connection(
